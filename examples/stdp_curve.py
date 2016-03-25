3e5c72ecb70a0eb589528f7b46e46d2a6daffd71
neworderofjamie <james.knight@manchester.ac.uk>
2016-03-25 15:34:15 +0000

    stdp curve example

---
+++ examples/stdp_curve.py
@@ -0,0 +1,99 @@
+import math, numpy, pylab, random, sys
+import pylab
+import pyNN.nest as sim
+from pyNN.parameters import Sequence
+
+#-------------------------------------------------------------------
+# This example uses the sPyNNaker implementation of pair-based STDP
+# To reproduce the eponymous STDP curve first
+# Plotted by Bi and Poo (1998)
+#-------------------------------------------------------------------
+
+#-------------------------------------------------------------------
+# Common parameters
+#-------------------------------------------------------------------
+time_between_pairs = 1000
+num_pairs = 60
+start_w = 0.5
+delta_t = [-100, -60, -40, -30, -20, -10, -1, 1, 10, 20, 30, 40, 60, 100]
+start_time = 200
+
+# Population parameters
+model = sim.IF_curr_exp
+cell_params = {'cm'        : 0.25, # nF
+               'i_offset'  : 0.0,
+               'tau_m'     : 10.0,
+               'tau_refrac': 2.0,
+               'tau_syn_E' : 2.5,
+               'tau_syn_I' : 2.5,
+               'v_reset'   : -70.0,
+               'v_rest'    : -65.0,
+               'v_thresh'  : -55.4}
+
+# SpiNNaker setup
+sim.setup(timestep=1.0, spike_precision="on_grid")
+sim_time = (num_pairs * time_between_pairs) + max(delta_t)
+
+# Neuron populations
+pre_pop = sim.Population(len(delta_t), model(**cell_params))
+post_pop = sim.Population(len(delta_t), model(**cell_params))
+
+# Stimulating populations
+pre_stim = sim.Population(len(delta_t), sim.SpikeSourceArray(spike_times=[10]))
+post_stim = sim.Population(len(delta_t), sim.SpikeSourceArray(spike_times=[10]))
+
+# Build stimulus spike times
+for i, t in enumerate(delta_t):
+    # Pre after post
+    if t > 0:
+        post_phase = start_time
+        pre_phase = start_time + t + 1
+    # Post after pre
+    else:
+        post_phase = start_time - t
+        pre_phase = start_time + 1
+
+    pre_stim[i].spike_times = Sequence(range(pre_phase, sim_time, time_between_pairs))
+    post_stim[i].spike_times = Sequence(range(post_phase, sim_time, time_between_pairs))
+
+# Connections between spike sources and neuron populations
+ee_connector = sim.OneToOneConnector()
+ee_synapse = sim.StaticSynapse(weight=2.0, delay=1.0)
+sim.Projection(pre_stim, pre_pop, ee_connector, ee_synapse)
+sim.Projection(post_stim, post_pop, ee_connector, ee_synapse)
+
+# Plastic Connection between pre_pop and post_pop
+stdp_model = sim.STDPMechanism(
+    timing_dependence=sim.SpikePairRule(tau_plus=16.7, tau_minus=33.7, A_plus=0.005, A_minus=0.005),
+    weight_dependence=sim.AdditiveWeightDependence(w_min=0.0, w_max=1.0),
+    weight=start_w, delay=1.0, dendritic_delay_fraction=1.0,
+)
+
+plastic_proj = sim.Projection(pre_pop, post_pop, sim.OneToOneConnector(), stdp_model)
+
+print("Simulating for %us" % (sim_time / 1000))
+
+# Run simulation
+sim.run(sim_time)
+
+# Get weight from each projection
+end_w = plastic_proj.getWeights()
+
+# End simulation on SpiNNaker
+sim.end()
+
+#-------------------------------------------------------------------
+# Plot curve
+#-------------------------------------------------------------------
+# Calculate deltas from end weights
+delta_w = [(w - start_w) / start_w for w in end_w]
+
+# Plot STDP curve
+figure, axis = pylab.subplots()
+axis.set_xlabel(r"$(t_{j} - t_{i}/ms)$")
+axis.set_ylabel(r"$(\frac{\Delta w_{ij}}{w_{ij}})$", rotation = "horizontal", size = "xx-large")
+axis.plot(delta_t, delta_w)
+axis.axhline(color = "grey", linestyle = "--")
+axis.axvline(color = "grey", linestyle = "--")
+
+pylab.show()
