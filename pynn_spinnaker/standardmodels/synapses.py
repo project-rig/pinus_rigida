d6732100ce5f29b676e3c1ac0f785a543a875df1
jamie <neworderofjamie@gmail.com>
2016-03-25 20:08:27 +0000

    In order to determine whether they could be simulated in the same synapse cluster, synapse models were previously compared only using their class i.e. StaticSynapse/STDPMechanism. However, with plasticity, all incoming connections must have the same learning rule parameters so synapse models must be compared more deeply. @mundya, @mossblaser I would much appreciate your feedback on whether this is a sane approach (it totally doesn't have to be until Tuesday if you're actually doing Easterish things). I worry about overriding __eq__ etc in this way - In C++/C# I would probably specify the comparison logic only for the container where I actually want this comparison behaviour.

--- pynn_spinnaker/standardmodels/synapses.py
+++ pynn_spinnaker/standardmodels/synapses.py
@@ -1,4 +1,5 @@
 from pyNN.standardmodels import synapses, build_translations
+from ..spinnaker import regions
 from ..simulator import state
 from ..spinnaker import lazy_param_map
 import logging
@@ -8,8 +9,53 @@ from functools import partial
 
 logger = logging.getLogger("PyNN")
 
+# ------------------------------------------------------------------------------
+# ComparisonMixin
+# ------------------------------------------------------------------------------
+# Mixin to provide comparison based on a tuple of object properties
+class ComparisonMixin(object):
+    def __eq__(self, other):
+        return self.compatible_comparison == other.compatible_comparison
 
-class StaticSynapse(synapses.StaticSynapse):
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __hash__(self):
+        return hash(self.compatible_comparison)
+
+# ------------------------------------------------------------------------------
+# ParamComparisonMixin
+# ------------------------------------------------------------------------------
+# Mixin to provide comparison based on a tuple of object properties
+class ParamComparisonMixin(ComparisonMixin):
+    @property
+    def compatible_comparison(self):
+        # Start tuple with class type - various STDP components
+        # are likely to have similarly named parameters
+        # with simular values so this is important 1st check
+        comp = (self.__class__,)
+
+        # Loop through names of parameters which
+        # much match for objects to be equal
+        for p in self.compatibility_param_names:
+            # Extract named parameter lazy array from parameter
+            # space and check that it's homogeneous
+            param_array = self.parameter_space[p]
+            assert param_array.is_homogeneous
+
+            # Set it's shape to 1
+            # **NOTE** for homogeneous arrays this is a)free and b)works
+            param_array.shape = 1
+
+            # Evaluate and simplify
+            # **NOTE** for homogeneous arrays this always returns a scalar
+            comp += (param_array.evaluate(simplify=True),)
+        return comp
+
+# ------------------------------------------------------------------------------
+# StaticSynapse
+# ------------------------------------------------------------------------------
+class StaticSynapse(synapses.StaticSynapse, ComparisonMixin):
     __doc__ = synapses.StaticSynapse.__doc__
     translations = build_translations(
         ("weight", "weight"),
@@ -35,7 +81,15 @@ class StaticSynapse(synapses.StaticSynapse):
             d = state.dt
         return d
 
-class STDPMechanism(synapses.STDPMechanism):
+    # Static synapses are always compatible with each other
+    @property
+    def compatible_comparison(self):
+        return (self.__class__,)
+
+# ------------------------------------------------------------------------------
+# STDPMechanism
+# ------------------------------------------------------------------------------
+class STDPMechanism(synapses.STDPMechanism, ComparisonMixin):
     __doc__ = synapses.STDPMechanism.__doc__
 
     base_translations = build_translations(
@@ -51,6 +105,8 @@ class STDPMechanism(synapses.STDPMechanism):
     # synapse_processor of this type process synaptic events (hZ)
     max_synaptic_event_rate = 2E6
 
+    plasticity_region_class = regions.Plasticity
+
     # How many timesteps of delay can DTCM ring-buffer handle
     # **NOTE** only 7 timesteps worth of delay can be handled by
     # 8 element delay buffer - The last element is purely for output
@@ -62,25 +118,45 @@ class STDPMechanism(synapses.STDPMechanism):
             d = state.dt
         return d
 
+    # STDP mechanisms should be compared based on their class, timing
+    # dependence (parameters) and weight dependence (parameters)
+    @property
+    def compatible_comparison(self):
+        return (self.__class__, self.timing_dependence,
+                self.weight_dependence)
 
-class AdditiveWeightDependence(synapses.AdditiveWeightDependence):
+    # The parameter map used to create the plasticity region is just the
+    # timing and weight dependence's parameter maps concatenated together
+    @property
+    def plasticity_param_map(self):
+        return self.timing_dependence.plasticity_param_map +\
+            self.weight_dependence.plasticity_param_map
+
+# ------------------------------------------------------------------------------
+# AdditiveWeightDependence
+# ------------------------------------------------------------------------------
+class AdditiveWeightDependence(synapses.AdditiveWeightDependence,
+                               ParamComparisonMixin):
     __doc__ = synapses.AdditiveWeightDependence.__doc__
 
     translations = build_translations(
         ("w_max",     "w_max"),
         ("w_min",     "w_min"),
-        ("A_plus",    "a_plus"),
-        ("A_minus",   "minus_a_minus",  "-A_minus", "-minus_a_minus"),
     )
 
-    param_map = [
+    plasticity_param_map = [
         ("w_min", "i4", lazy_param_map.unsigned_weight_fixed_point),
         ("w_max", "i4", lazy_param_map.unsigned_weight_fixed_point),
         ("a_plus", "i4", lazy_param_map.s2211),
-        ("minus_a_minus", "i4", lazy_param_map.s2211),
+        ("a_minus", "i4", lazy_param_map.s2211),
     ]
 
-class SpikePairRule(synapses.SpikePairRule):
+    compatibility_param_names =  ("w_max", "w_min")
+
+# ------------------------------------------------------------------------------
+# SpikePairRule
+# ------------------------------------------------------------------------------
+class SpikePairRule(synapses.SpikePairRule, ParamComparisonMixin):
     __doc__ = synapses.SpikePairRule.__doc__
 
     translations = build_translations(
@@ -90,7 +166,11 @@ class SpikePairRule(synapses.SpikePairRule):
         ("A_minus",   "a_minus"),
     )
 
-    param_map = [
-        ("tau_plus", "256i2", partial(lazy_param_map.unsigned_weight_fixed_point, num_entries=256, time_shift=0)),
-        ("tau_minus", "256i2", partial(lazy_param_map.unsigned_weight_fixed_point, num_entries=256, time_shift=0)),
+    plasticity_param_map = [
+        ("tau_plus", "256i2", partial(lazy_param_map.s411_exp_decay_lut,
+                                      num_entries=256, time_shift=0)),
+        ("tau_minus", "256i2", partial(lazy_param_map.s411_exp_decay_lut,
+                                       num_entries=256, time_shift=0)),
     ]
+
+    compatibility_param_names = ("tau_plus", "tau_minus", "A_plus", "A_minus")
