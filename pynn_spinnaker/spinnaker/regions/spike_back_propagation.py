8d08f4bbe0ff81d82d6cac3031f8ba159f8f1d9e
jamie <neworderofjamie@gmail.com>
2016-03-28 22:03:07 +0100

    start of spike back propagation region

---
+++ pynn_spinnaker/spinnaker/regions/spike_back_propagation.py
@@ -0,0 +1,46 @@
+# Import modules
+import logging
+import numpy as np
+
+# Import classes
+from region import Region
+
+logger = logging.getLogger("pynn_spinnaker")
+
+# ------------------------------------------------------------------------------
+# SpikeBackPropagation
+# ------------------------------------------------------------------------------
+class SpikeBackPropagation(Region):
+    # --------------------------------------------------------------------------
+    # Region methods
+    # --------------------------------------------------------------------------
+    def sizeof(self, vertex_slice):
+        """Get the size requirements of the region in bytes.
+
+        Parameters
+        ----------
+        vertex_slice : :py:func:`slice`
+            A slice object which indicates which rows, columns or other
+            elements of the region should be included.
+
+        Returns
+        -------
+        int
+            The number of bytes required to store the data in the given slice
+            of the region.
+        """
+        # 8 words
+        return 4 * 8
+
+    def write_subregion_to_file(self, fp, vertex_slice):
+        """Write a portion of the region to a file applying the formatter.
+
+        Parameters
+        ----------
+        fp : file-like object
+            The file-like object to which data from the region will be written.
+            This must support a `write` method.
+        vertex_slice : :py:func:`slice`
+            A slice object which indicates which rows, columns or other
+            elements of the region should be included.
+        """
