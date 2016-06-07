d477f15b41dc7cf908d865e502f93e7127d52c56
neworderofjamie <james.knight@manchester.ac.uk>
2016-06-07 17:38:46 +0100

    Connector testing start

---
+++ tests/test_connectors.py
@@ -0,0 +1,62 @@
+# Import modules
+import numpy as np
+import pytest
+import pynn_spinnaker as sim
+
+# Import classes
+from pynn_spinnaker.spinnaker.synapse_cluster import WeightRange
+from pynn_spinnaker.spinnaker.utils import UnitStrideSlice
+
+# ----------------------------------------------------------------------------
+# Tests
+# ----------------------------------------------------------------------------
+@pytest.mark.parametrize("pre_size", [1000])
+@pytest.mark.parametrize("post_size, post_slice",
+                         [(5000, UnitStrideSlice(0, 1024)),
+                          (5000, UnitStrideSlice(1024, 2048)),
+                          (5000, UnitStrideSlice(3976, 5000))])
+@pytest.mark.parametrize("connector",
+                         [sim.AllToAllConnector(),
+                          sim.OneToOneConnector(),
+                          sim.FixedProbabilityConnector(0.1),
+                          sim.FixedProbabilityConnector(0.5),
+                          sim.FixedProbabilityConnector(1.0),
+                          sim.FixedNumberPreConnector(10),
+                          sim.FixedNumberPostConnector(100),
+                          sim.FixedNumberPostConnector(1000),])
+def test_estimate_max_row_synapses(pre_size, post_size, post_slice, connector):
+    # Setup simulator
+    sim.setup(timestep=1.0, min_delay=1.0, max_delay=8.0, spinnaker_hostname="")
+
+    # Create two populations
+    pre = sim.Population(pre_size, sim.IF_curr_exp())
+    post = sim.Population(post_size, sim.IF_curr_exp())
+
+    # Connect the populations together
+    proj = sim.Projection(pre, post, connector, sim.StaticSynapse())
+
+    # Estimate maximum row length
+    estimated_max_row_synapses = proj._estimate_max_row_synapses(
+        UnitStrideSlice(0, pre_size), post_slice)
+
+    # Create list of lists to contain matrix rows
+    sub_rows = [[] for _ in range(pre_size)]
+
+    # Create weight range
+    weight_range = WeightRange(sim.StaticSynapse.signed_weight)
+
+    # Local mask to select only the columns
+    # corresponding to neurons in postsynaptic vertex
+    proj.post._mask_local = np.zeros((post_size,), dtype=bool)
+    proj.post._mask_local[post_slice.python_slice] = True
+
+    # Build projection
+    proj._build(matrix_rows=sub_rows,
+                weight_range=weight_range,
+                directly_connect=False)
+
+    # Calculate actual maximum row length
+    actual_max_row_synapses = max(len(s) for s in sub_rows)
+
+    assert estimated_max_row_synapses >= actual_max_row_synapses
+    assert estimated_max_row_synapses < actual_max_row_synapses * 1.25
