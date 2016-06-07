c2326fc4c8392da7a544f540ddd19143e13684ca
neworderofjamie <james.knight@manchester.ac.uk>
2016-06-07 17:03:12 +0100

    estimate_max_row_synapses functions for full set of connectors

--- pynn_spinnaker/connectors.py
+++ pynn_spinnaker/connectors.py
@@ -52,7 +52,8 @@ class FixedProbabilityConnector(FixedProbabilityConnector):
         # Create array of possible row lengths
         x = np.arange(len(post_slice))
 
-        # Calculate CDF
+        # Calculate CDF of binomial distribution representing len(post_slice)
+        # number of events with p_connect probability
         cdf = scipy.stats.binom.cdf(x, len(post_slice), self.p_connect)
 
         # Return row-length corresponding to 99.9% of rows
@@ -91,9 +92,17 @@ class FromListConnector(FromListConnector):
         pre_indices = self.conn_list[:, 0]
         post_indices = self.conn_list[:, 1]
 
-        # Compute histogram
-        #bins = np.histogram(len(pre_slice))[0]
-        raise NotImplementedError()
+        # Build mask to select list entries in slice
+        mask = ((pre_indices >= pre_slice.start) &
+                (pre_indices < pre_slice.stop) &
+                (post_indices >= post_slice.start) &
+                (post_indices < post_slice.stop))
+
+        # Use mask to select slice pre-indices
+        slice_pre_indices = pre_indices[mask].astype(int)
+
+        # Return maximum number of list entries in each bin
+        return np.amax(np.bincount(slice_pre_indices));
 
     def estimate_num_synapses(self, pre_slice, post_slice,
                               pre_size, post_size):
@@ -117,7 +126,19 @@ class FixedNumberPostConnector(FixedNumberPostConnector):
 
     def estimate_max_row_synapses(self, pre_slice, post_slice,
                                   pre_size, post_size):
-        raise NotImplementedError()
+        # Create array of possible row lengths - there can't be more than n!
+        x = np.arange(self.n)
+
+        # Calculate the probability that any of the
+        # n synapses in the row is within post-slice
+        prob_in_row = float(len(post_slice)) / float(post_size)
+
+        # Calculate CDF of binomial distribution representing n
+        # events with prob_in_slice probability of being in post_slice
+        cdf = scipy.stats.binom.cdf(x, self.n, prob_in_row)
+
+        # Return row-length corresponding to 99.9% of rows
+        return min(len(post_slice), np.searchsorted(cdf, 0.999))
 
     def estimate_num_synapses(self, pre_slice, post_slice,
                               pre_size, post_size):
@@ -151,7 +172,20 @@ class FixedTotalNumberConnector(FixedTotalNumberConnector):
 
     def estimate_max_row_synapses(self, pre_slice, post_slice,
                                   pre_size, post_size):
-        raise NotImplementedError()
+        # Create array of possible row lengths - there can't be more than n!
+        x = np.arange(self.n)
+
+        # Calculate the probability that any of the n synapses
+        # in the full matrix are within post-slice of a single row
+        prob_in_row = (1.0 / float(pre_size)) *\
+            (float(len(post_slice)) / float(post_size))
+
+        # Calculate CDF of binomial distribution representing n events with
+        # prob_in_row probability of being in slice
+        cdf = scipy.stats.binom.cdf(x, self.n, prob_in_row)
+
+        # Return row-length corresponding to 99.9% of rows
+        return min(len(post_slice), np.searchsorted(cdf, 0.999))
 
     def estimate_num_synapses(self, pre_slice, post_slice,
                               pre_size, post_size):
