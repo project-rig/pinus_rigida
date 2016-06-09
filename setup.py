40b3232ee42a560f59e4bc480ab490cc5768abff
neworderofjamie <james.knight@manchester.ac.uk>
2016-06-09 11:17:01 +0100

    Increased version to 0.3.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def replace_local_hyperlinks(
 
 setup(
     name="pynn_spinnaker",
-    version="0.2.3",
+    version="0.3.0",
     packages=find_packages(),
     package_data={'pynn_spinnaker': ['model_binaries/*.aplx']},
 
