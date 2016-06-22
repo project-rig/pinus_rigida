2d28fc3df995013a6f66aea3d43a7cc303973d97
neworderofjamie <james.knight@manchester.ac.uk>
2016-06-22 10:51:38 +0100

    Updated connector test to test mean row lengths

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -35,9 +35,11 @@ def test_estimate_max_row_synapses(pre_size, post_size, post_slice, connector):
     # Connect the populations together
     proj = sim.Projection(pre, post, connector, sim.StaticSynapse())
 
-    # Estimate maximum row length
+    # Estimate maximum and mean row length
     estimated_max_row_synapses = proj._estimate_max_row_synapses(
         UnitStrideSlice(0, pre_size), post_slice)
+    estimated_mean_row_synapses = proj._connector._estimate_mean_row_synapses(
+            UnitStrideSlice(0, pre_size), pre_size, post_size)
 
     # Create list of lists to contain matrix rows
     sub_rows = [[] for _ in range(pre_size)]
@@ -55,11 +57,16 @@ def test_estimate_max_row_synapses(pre_size, post_size, post_slice, connector):
                 weight_range=weight_range,
                 directly_connect=False)
 
-    # Calculate actual maximum row length
+    # Calculate actual maximum and mean row length
     actual_max_row_synapses = max(len(s) for s in sub_rows)
+    actual_mean_row_synapses = sum(len(s) for s in sub_rows) / float(len(sub_rows))
 
     # Check estimated maximum is greater or equal than actual maximum
     assert estimated_max_row_synapses >= actual_max_row_synapses
 
     # Check estimated maximum isn't TOO big
     assert estimated_max_row_synapses <= actual_max_row_synapses * 1.25
+
+    # Check estimated mean is approximately correct
+    assert (estimated_mean_row_synapses / actual_mean_row_synapses) < 1.25
+    assert (estimated_mean_row_synapses / actual_mean_row_synapses) > 0.75
