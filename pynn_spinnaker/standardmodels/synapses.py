0cf22e95a110a9c3fee13c1b708edec4645e7762
jamie <neworderofjamie@gmail.com>
2016-07-07 04:45:39 +0100

    Updated STDP synapse to support PyNN fix

--- pynn_spinnaker/standardmodels/synapses.py
+++ pynn_spinnaker/standardmodels/synapses.py
@@ -64,7 +64,8 @@ class STDPMechanism(synapses.STDPMechanism):
 
     base_translations = build_translations(
         ("weight", "weight"),
-        ("delay", "delay")
+        ("delay", "delay"),
+        ("dendritic_delay_fraction", "dendritic_delay_fraction")
     )
 
     def _get_minimum_delay(self):
