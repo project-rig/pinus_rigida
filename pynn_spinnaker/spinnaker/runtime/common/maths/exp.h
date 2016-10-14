1b914b1a80c6bef119a473f21da4675cc66ef72d
neworderofjamie <james.knight@manchester.ac.uk>
2016-10-14 15:07:42 +0100

    Exponential

---
+++ pynn_spinnaker/spinnaker/runtime/common/maths/exp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Common includes
+#include "../fixed_point_number.h"
+
+// Namespaces
+using namespace Common::FixedPointNumber;
+
+//-----------------------------------------------------------------------------
+// Common::Maths
+//-----------------------------------------------------------------------------
+namespace Common
+{
+namespace Maths
+{
+  S1615 ExpS1615(S1615 v);
+} // Maths
+} // Commo
