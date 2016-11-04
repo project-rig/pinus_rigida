65349561261bf9c917686d3fa350175003f08106
neworderofjamie <james.knight@manchester.ac.uk>
2016-11-04 15:39:39 +0000

    Bug-fix so stats aren't attempted to be read from non-existant neural clusters

--- pynn_spinnaker/simulator.py
+++ pynn_spinnaker/simulator.py
@@ -228,12 +228,14 @@ class State(common.control.BaseState):
                 logger.info("\t\t\tKey lookup failures:%u",
                             np.sum(stats["key_lookup_fails"]))
 
-            neural_stats = pop.get_neural_statistics()
-            logger.info("\t\tNeurons")
-            logger.info("\t\t\tTask queue overflows:%u",
-                            np.sum(neural_stats["task_queue_full"]))
-            logger.info("\t\t\tTimer event overruns:%u",
-                            np.sum(neural_stats["timer_event_overflows"]))
+            # If population has a neural cluster
+            if pop._neural_cluster is not None:
+                neural_stats = pop.get_neural_statistics()
+                logger.info("\t\tNeurons")
+                logger.info("\t\t\tTask queue overflows:%u",
+                                np.sum(neural_stats["task_queue_full"]))
+                logger.info("\t\t\tTimer event overruns:%u",
+                                np.sum(neural_stats["timer_event_overflows"]))
 
     def _build(self, duration_ms):
         # Convert dt into microseconds and divide by
