217d6d3ee5a67f58c49b7099304f3e32acb77d91
neworderofjamie <james.knight@manchester.ac.uk>
2016-11-17 17:15:09 +0000

    Improved delay buffer size estimation * SynapticMatrix provides an estimate of the number of delay rows per second processing each submatrix will result in * Delay buffer sums these and scales into timesteps

--- pynn_spinnaker/spinnaker/regions/delay_buffer.py
+++ pynn_spinnaker/spinnaker/regions/delay_buffer.py
@@ -1,23 +1,25 @@
 # Import modules
+import logging
 import math
 import struct
 
 # Import classes
 from rig_cpp_common.regions import Region
 
+logger = logging.getLogger("pynn_spinnaker")
+
 # ------------------------------------------------------------------------------
 # DelayBuffer
 # ------------------------------------------------------------------------------
 class DelayBuffer(Region):
-    def __init__(self, max_synaptic_event_rate, sim_timestep_ms, max_delay_ms):
+    def __init__(self, sim_timestep_ms, max_delay_ms):
         # Calculate maximum delay in ticks and
         # round up to next largest power-of-two
         self.max_delay_ticks = int(math.ceil(max_delay_ms / sim_timestep_ms))
         self.max_delay_ticks = 2 ** (self.max_delay_ticks - 1).bit_length()
 
-        # Scale synaptic event rate into timer ticks
-        self.max_events_per_tick =\
-            max_synaptic_event_rate * (float(sim_timestep_ms) / 1000.0)
+        # Convert simulation timestep to seconds and cache
+        self.sim_timestep_s = float(sim_timestep_ms) / 1000.0
 
 
     # --------------------------------------------------------------------------
@@ -64,10 +66,16 @@ class DelayBuffer(Region):
         fp.write(struct.pack("II", self.max_delay_ticks, buffer_size))
 
     def _calc_buffer_size(self, sub_matrix_props):
-        # **TODO** is there a point to capping this?
+        # Calculate the total number of delay rows per second
+        # this synapse processor is going to have to handle
+        delay_rows_per_second = sum(s.max_delay_rows_per_second
+                                    for s in sub_matrix_props)
 
-        # Get the maximum number of columns in any sub-matrices
-        max_cols = max(s.max_cols for s in sub_matrix_props)
+        # Scale this to obtain the number of delay rows per second
+        delay_rows_per_timestep = int(math.ceil(delay_rows_per_second *
+                                                self.sim_timestep_s))
+        logger.debug("\t\tDelay rows per-second:%f, per timestep:%u",
+                     delay_rows_per_second, delay_rows_per_timestep)
 
-        # Use this to scale events per tick into rows per tick
-        return max(1, min(255, self.max_events_per_tick // max_cols))
+        # Clamp this above 1 to ensure that memory is always allocated
+        return max(1, delay_rows_per_timestep)
